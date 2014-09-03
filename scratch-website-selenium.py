c77c94dccc57fd54e97c80d0ca71198f8efaf8e8
Chris Keeley <pickledjesus@gmail.com>
2014-09-03 20:03:54 +0100

    Added try, except block that closes and creates a new webdriver if ElementNotVisibleException occurs

--- scratch-website-selenium.py
+++ scratch-website-selenium.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.select import Select
+from selenium.common.exceptions import ElementNotVisibleException
 import csv
 import logging
 
@@ -12,43 +13,54 @@ monthDict={'01':"January", '02':"February", '03':"March", '04':"April",
 
 with open("students.csv", 'rb') as file:
     students = csv.reader(file)
-          
+    
     driver = webdriver.Chrome()
     driver.get("http://scratch.mit.edu")
 
     for student in students:
-        menu = driver.find_element_by_class_name("join-scratch")
-        menu.click()
+        completed = False
+        while not completed:
+            try:       
+                menu = driver.find_element_by_class_name("join-scratch")
+                menu.click()
 
-        driver.implicitly_wait(5)
+                driver.implicitly_wait(10)
 
-        username = driver.find_element_by_xpath("(//input[@name='username'])[3]")
-        username.send_keys(student[0].replace('.', '-'))
-        password = driver.find_element_by_xpath("(//input[@name='password'])[3]")
-        password.send_keys(student[1])
-        password_confirm = driver.find_element_by_name("password-confirm")
-        password_confirm.send_keys(student[1])
+                username = driver.find_element_by_xpath("(//input[@name='username'])[3]")
+                username.send_keys(student[0].replace('.', '-'))
+                password = driver.find_element_by_xpath("(//input[@name='password'])[3]")
+                password.send_keys(student[1])
+                password_confirm = driver.find_element_by_name("password-confirm")
+                password_confirm.send_keys(student[1])
 
-        next_button = driver.find_element_by_id("registration-next")
-        next_button.click()
+                next_button = driver.find_element_by_id("registration-next")
+                next_button.click()
 
-        birth_month = Select(driver.find_element_by_name("birthmonth"))
-        birth_month.select_by_visible_text(monthDict[student[3][3:5]])
-        birth_year = Select(driver.find_element_by_name("birthyear"))
-        birth_year.select_by_visible_text(student[3][6:])
-        gender = driver.find_element_by_id("gender_other_radio")
-        gender.click()
-        country = Select(driver.find_element_by_name("country"))
-        country.select_by_visible_text("United Kingdom")
-        email = driver.find_element_by_name("email")
-        email.send_keys(student[2])
+                birth_month = Select(driver.find_element_by_name("birthmonth"))
+                birth_month.select_by_visible_text(monthDict[student[3][3:5]])
+                birth_year = Select(driver.find_element_by_name("birthyear"))
+                birth_year.select_by_visible_text(student[3][6:])
+                gender = driver.find_element_by_id("gender_other_radio")
+                gender.click()
+                country = Select(driver.find_element_by_name("country"))
+                country.select_by_visible_text("United Kingdom")
+                email = driver.find_element_by_name("email")
+                email.send_keys(student[2])
 
-        next_button.click()
-        
-        logging.info(student[0].replace('.', '-') + " successfully created")
-        #finish_button = driver.find_element_by_id("registration-done")
-        #finish_button.click()
+                next_button.click()
+                
+                logging.info(student[0].replace('.', '-') + " successfully created")
+                
+                driver.delete_all_cookies()
+                driver.refresh()
+                
+            except ElementNotVisibleException:
+                driver.close()
+                driver = webdriver.Chrome()
+                driver.get("http://scratch.mit.edu")
+                print "ElementNotVisibleException"
+                print student
+            else:
+                completed = True
 
-        driver.delete_all_cookies()
-        driver.refresh()
 
