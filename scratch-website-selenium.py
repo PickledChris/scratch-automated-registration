8912516047b32c6a23662214daef610a349077b9
Chris Keeley <pickledjesus@gmail.com>
2014-09-03 16:12:17 +0100

    Working script that reads from a csv file. Now uses Chrome to improve performance and stability but still crashes occasionally. Logs to output file for manual resuming.

--- scratch-website-selenium.py
+++ scratch-website-selenium.py
@@ -1,41 +1,54 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.select import Select
+import csv
+import logging
 
-driver = webdriver.Firefox()
-driver.get("http://scratch.mit.edu")
+logger = logging.basicConfig(filename="students-created.log", level=logging.INFO)
 
-menu = driver.find_element_by_class_name("join-scratch")
-menu.click()
+monthDict={'01':"January", '02':"February", '03':"March", '04':"April",
+           '05':"May", '06':"June", '07':"July", '08':"August",
+           '09':"September", '10':"October", '11':"November", '12':"December"}
 
-driver.implicitly_wait(5)
+with open("students.csv", 'rb') as file:
+    students = csv.reader(file)
+          
+    driver = webdriver.Chrome()
+    driver.get("http://scratch.mit.edu")
 
-username = driver.find_element_by_xpath("(//input[@name='username'])[3]")
-username.send_keys("benhurst1014")
-password = driver.find_element_by_xpath("(//input[@name='password'])[3]")
-password.send_keys("password1")
-password_confirm = driver.find_element_by_name("password-confirm")
-password_confirm.send_keys("password1")
+    for student in students:
+        menu = driver.find_element_by_class_name("join-scratch")
+        menu.click()
 
-next_button = driver.find_element_by_id("registration-next")
-next_button.click()
+        driver.implicitly_wait(5)
 
-birth_month = Select(driver.find_element_by_name("birthmonth"))
-birth_month.select_by_visible_text("January")
-birth_year = Select(driver.find_element_by_name("birthyear"))
-birth_year.select_by_visible_text("1990")
-gender = driver.find_element_by_name("gender")
-gender.click()
-country = Select(driver.find_element_by_name("country"))
-country.select_by_visible_text("United Kingdom")
-email = driver.find_element_by_name("email")
-email.send_keys("benhurst1010@bugmenot.com")
+        username = driver.find_element_by_xpath("(//input[@name='username'])[3]")
+        username.send_keys(student[0].replace('.', '-'))
+        password = driver.find_element_by_xpath("(//input[@name='password'])[3]")
+        password.send_keys(student[1])
+        password_confirm = driver.find_element_by_name("password-confirm")
+        password_confirm.send_keys(student[1])
 
-next_button.click()
+        next_button = driver.find_element_by_id("registration-next")
+        next_button.click()
 
-finish_button = driver.find_element_by_id("registration-done")
-finish_button.click()
+        birth_month = Select(driver.find_element_by_name("birthmonth"))
+        birth_month.select_by_visible_text(monthDict[student[3][3:5]])
+        birth_year = Select(driver.find_element_by_name("birthyear"))
+        birth_year.select_by_visible_text(student[3][6:])
+        gender = driver.find_element_by_id("gender_other_radio")
+        gender.click()
+        country = Select(driver.find_element_by_name("country"))
+        country.select_by_visible_text("United Kingdom")
+        email = driver.find_element_by_name("email")
+        email.send_keys(student[2])
 
-driver.delete_all_cookies()
-driver.refresh()
+        next_button.click()
+        
+        logging.info(student[0].replace('.', '-') + " successfully created")
+        #finish_button = driver.find_element_by_id("registration-done")
+        #finish_button.click()
+
+        driver.delete_all_cookies()
+        driver.refresh()
 
